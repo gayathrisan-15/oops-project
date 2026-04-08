ae0a59666541f237eb59305a954f2849f188d2b6
nandanasajith8787 <nandanasajith8787@gmail.com>
2026-04-09 01:52:50 +0530

    Adding headers and entry point

---
+++ face.cpp
@@ -0,0 +1,11 @@
+// Face Detection + Mask Alert System
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <chrono>
+using namespace std::chrono;
+
+using namespace cv;
+using namespace std;
+
+int main() {
+}
